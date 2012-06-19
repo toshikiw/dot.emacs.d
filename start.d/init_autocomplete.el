fd0aa9fdb25479baf061d32842e3e00872462a25
Toshiki Watanabe <watanabe@exmedia.jp>
2012-06-19 17:47:15 +0900

    add eshell complete setting

--- start.d/init_autocomplete.el
+++ start.d/init_autocomplete.el
@@ -8,4 +8,13 @@
 ;; 自動補完のキー割当て
 (define-key ac-mode-map (kbd "M-/") 'auto-complete)
 
+;; eshell setting
+(require 'pcomplete)
+(add-to-list 'ac-modes 'eshell-mode)
+(ac-define-source pcomplete
+  '((candidates . pcomplete-completions))
+)
+;(defun my-ac-eshell-mode ()
+;  (setq ac-sources
+;        '()))
 (provide 'init_autocomplete)
