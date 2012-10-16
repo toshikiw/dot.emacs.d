158dcea9807c5004bfd9b5b8fecb19f6d7356292
Toshiki Watanabe <watanabe@exmedia.jp>
2012-10-16 17:06:30 +0900

    checked at installing package

--- start.d/init_anything.el
+++ start.d/init_anything.el
@@ -1,21 +1,28 @@
 ;; anything
-(require 'anything-config)
-;(require 'anything-startup)
-(global-set-key (kbd "C-;") 'anything)
-(setq anything-sources
-      '(
-        anything-c-source-buffers
-        anything-c-source-files-in-current-dir
-        anything-c-source-file-name-history
-        anything-c-source-filelist
-        anything-c-source-locate
-        anything-c-source-file-cache
-        anything-c-source-bookmarks
-        anything-c-source-emacs-commands
+(when (require 'anything-config nil t)
+  ;(require 'anything-startup)
+  (global-set-key (kbd "C-;") 'anything)
+  (setq anything-sources
+        '(
+          anything-c-source-buffers
+          anything-c-source-files-in-current-dir
+          anything-c-source-file-name-history
+          anything-c-source-filelist
+          anything-c-source-locate
+          anything-c-source-file-cache
+          anything-c-source-bookmarks
+          anything-c-source-emacs-commands
+          )
         )
+  ;; recentf
+  (require 'recentf)
+  (setq recentf-max-saved-items 1000)
+  (recentf-mode 1)
+  ;; locate db
+  (let ((locatedb (concat (getenv "HOME") "/Library/Caches/com.apple.locate.db")))
+    (when (file-readable-p locatedb)
+      (setenv "LOCATE_PATH" locatedb)
+      )
+    )
 )
-;; recentf
-(require 'recentf)
-(setq recentf-max-saved-items 1000)
-(recentf-mode 1)
 (provide 'init_anything)
--- start.d/init_marmalade.el
+++ start.d/init_marmalade.el
@@ -1,5 +1,14 @@
-(require 'package)
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/"))
-(package-initialize)
+(when (require 'package nil t)
+  (setq package-archives 
+        '(
+          ("marmalade" . "http://marmalade-repo.org/packages/")
+          ("ELPA" . "http://tromey.com/elpa/")
+          ("GNU" . "http://elpa.gnu.org/packages/")
+          ))
+        ;(append (list
+        ;         '("ELPA" . "http://tromey.com/elpa/")
+        ;         '("marmalade" . "http://marmalade-repo.org/packages/"))
+        ;        package-archives))
+  (package-initialize)
+)
 (provide 'init_marmalade)
